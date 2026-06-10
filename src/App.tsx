bb8ce3aabdb9f7292aa667cdaa1d65a5ded1462a
Barwolf <jeremiahlillion@gmail.com>
2026-06-09 17:29:57 -0700

    makes pages scroll to the top

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,23 @@ import { Home } from "./pages/Home";
 import { Resume } from "./pages/Resume";
 import { Tech } from "./pages/Tech";
 
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  
+  return null;
+}
+
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
